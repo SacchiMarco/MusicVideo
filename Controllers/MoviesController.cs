a3d39511504fac54c77c363960728e53f7a19c6a
Marco Sacchi <firestorms@bluewin.ch>
2019-10-30 23:08:31 +0100

    Editing a Movie finished

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 
+
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
@@ -50,7 +51,7 @@ namespace Vidly.Controllers
         public ActionResult SaveMovie(int id)
         {
             var viewModel = new MoviesViewModel();
-            if (id == 0)
+            if (id <= 0)
             {
                 viewModel.KuenstlerList = _context.Kuenstler;
                 viewModel.KategorienList = _context.Kategorien;
@@ -60,7 +61,13 @@ namespace Vidly.Controllers
             }
             else
             {
-                return View();
+
+                viewModel.Videos = _context.Videos.Single(v => v.v_id == id);
+                viewModel.KuenstlerList = _context.Kuenstler;
+                viewModel.KategorienList = _context.Kategorien;
+                viewModel.ViewTitel = "Edit this Movie";
+
+                return View(viewModel);
             }
         }
 
@@ -72,10 +79,27 @@ namespace Vidly.Controllers
                 _context.Videos.Add(formPosts.Videos);
                 _context.SaveChanges();
 
-                
+                return Redirect("Movies");
             }
+            else
+            {
+                var videoInDb = _context.Videos.Single(v => v.v_id == formPosts.Videos.v_id);
 
-            return Redirect("Movies");
+                videoInDb.album = formPosts.Videos.album;
+                videoInDb.anmerkung = formPosts.Videos.anmerkung;
+                videoInDb.benotung = formPosts.Videos.benotung;
+                videoInDb.dateigroesse = formPosts.Videos.dateigroesse;
+                videoInDb.erscheinungsjahr = formPosts.Videos.erscheinungsjahr;
+                videoInDb.fk_a_id = formPosts.Videos.fk_a_id;
+                videoInDb.fk_k_id = formPosts.Videos.fk_k_id;
+                videoInDb.qualitaet = formPosts.Videos.qualitaet;
+                videoInDb.titel = formPosts.Videos.titel;
+
+
+                _context.SaveChanges();
+
+                return Redirect("Movies");
+            }
         }
 
     }
